ab1c6e00bb4a2a60f724006bc8f42e2d9747738a
Massimo <massimo@syneforge.com>
2019-01-11 13:17:42 +0200

    fixed return behaviour of callMethod; now it returns Promise as expected and when makes call to client follow client nature and uses callback

--- test/AdvegoIntegrationModule.test.js
+++ test/AdvegoIntegrationModule.test.js
@@ -1,11 +1,19 @@
 const AdvegoIntegrationModule = require('../lib/AdvegoIntegrationModule');
 const SimpleConfigurationsManager = require('../lib/ConfigurationsManager/SimpleConfigurationsManager');
 
+let clientReturnValue;
+
 class MokedConfigurationConstructor {
   constructor({ host }) {
     this.host = host;
     this.queues = {
-      getJobsAll: () => 'test',
+      getJobsAll: (options, callback) => {
+        if (clientReturnValue) {
+          callback(null, clientReturnValue);
+        } else {
+          callback(true);
+        }
+      },
     };
   }
 }
@@ -17,6 +25,7 @@ describe('[AdvegoIntegrationModule]', () => {
   beforeEach(() => {
     aim = new AdvegoIntegrationModule(manager);
     aim.reset();
+    clientReturnValue = undefined;
   });
 
   test('Should create instance', () => {
@@ -70,5 +79,63 @@ describe('[AdvegoIntegrationModule]', () => {
       method: 'getJobsAll',
     });
     expect(spy).toHaveBeenCalledWith('super');
+  });
+
+
+  test('Should resolve Promise with response if all OK on callMethod', (done) => {
+    const config = {
+      name: 'super',
+      host: 'localhost',
+      port: 4000,
+      path: '/xml',
+    };
+    aim.updateConfiguration(config);
+    clientReturnValue = { jobs: [] };
+    aim.callMethod({
+      configurationName: 'super',
+      method: 'getJobsAll',
+    })
+      .then((response) => {
+        expect(response).toEqual(clientReturnValue);
+        done();
+      });
+  });
+
+  test('Should reject Promise with error if response has error property on callMethod', (done) => {
+    const config = {
+      name: 'super',
+      host: 'localhost',
+      port: 4000,
+      path: '/xml',
+    };
+    aim.updateConfiguration(config);
+    clientReturnValue = { error: 1 };
+    aim.callMethod({
+      configurationName: 'super',
+      method: 'getJobsAll',
+    })
+      .catch((response) => {
+        expect(response).toEqual(clientReturnValue.error);
+        done();
+      });
+  });
+
+  test('Should reject Promise with response if response has has_errors property on callMethod', (done) => {
+    const config = {
+      name: 'super',
+      host: 'localhost',
+      port: 4000,
+      path: '/xml',
+    };
+    aim.updateConfiguration(config);
+    clientReturnValue = { has_errors: 1 };
+    aim.callMethod({
+      configurationName: 'super',
+      method: 'getJobsAll',
+    })
+      .catch((response) => {
+        expect(response).toEqual(clientReturnValue);
+        done();
+      });
   });
 });
