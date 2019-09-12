f9e76021604df994bb2398ed4da0532270054e4c
Duck <dmitriy.skachko2@gmail.com>
2019-09-12 09:27:47 +0300

    chore: add getJobsByOrderId test

---
+++ test/GetJobsByOrderId.test.js
@@ -0,0 +1,57 @@
+const GetJobsByOrderId = require("../lib/Client/HttpRPCXMLClient/GetJobsByOrderId");
+
+describe("[GetJobsByOrderId]", () => {
+  let getJobsByOrderId;
+  const clientResponse = {};
+  const client = {
+    methodCall: (method, options, cb) => {
+      if (clientResponse.success) {
+        return cb(null, clientResponse.payload);
+      }
+      return cb(clientResponse.payload);
+    }
+  };
+  const xmlrpc = {
+    createSecureClient: () => ({ methodCall: client.methodCall })
+  };
+  const errorChecker = jest.fn();
+
+  beforeEach(() => {
+    Object.assign(clientResponse, { success: true, payload: { id: 1 } });
+    getJobsByOrderId = GetJobsByOrderId(xmlrpc, errorChecker);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should be a function", () => {
+    expect(typeof getJobsByOrderId).toStrictEqual("function");
+  });
+
+  it("should call xmlrpc.createSecureClient", async () => {
+    const options = {
+      host: 1,
+      port: 1,
+      path: 1,
+      token: 1
+    };
+    const spy = jest.spyOn(xmlrpc, "createSecureClient");
+    await getJobsByOrderId(options);
+    expect(spy).toHaveBeenCalledWith(options);
+  });
+
+  it("should call client.methodCall with advego.getJobsAll and order options", async () => {
+    const options = { id: 1 };
+    const spy = jest.spyOn(client, "methodCall");
+    await getJobsByOrderId(options);
+    expect(spy.mock.calls[0][0]).toStrictEqual("advego.getJobsAll");
+    expect(spy.mock.calls[0][1]).toStrictEqual([{ id: 1 }]);
+  });
+
+  it("should call errorChecker", async () => {
+    const options = { id: 1 };
+    await getJobsByOrderId(options);
+    expect(errorChecker).toHaveBeenCalledWith({ id: 1 });
+  });
+});
